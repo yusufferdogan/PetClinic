dc00e2ba5e2eb699f56e890d0c1b10cfacd97f76
yusuf erdogan <a.erdogan2018@gtu.edu.tr>
2021-08-09 15:31:41 +0300

    Initial commit

---
+++ src/test/java/com/yusuferdogan/petclinic/PetclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yusuferdogan.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetclinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
