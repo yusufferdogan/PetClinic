482fc09a186904bb09a636742f99baa105891829
yusuf erdogan <a.erdogan2018@gtu.edu.tr>
2021-08-16 15:03:02 +0300

    adding POJOs

---
+++ src/main/java/com/yusuferdogan/petclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.yusuferdogan.petclinic.model;
+
+public class Owner extends Person{
+}
---
+++ src/main/java/com/yusuferdogan/petclinic/model/Vet.java
@@ -0,0 +1,6 @@
+package com.yusuferdogan.petclinic.model;
+
+public class Vet extends Person{
+
+
+}
