d9b664cf5dbcf40609f16514f2e5dbe144f4c7b1
yusuf erdogan <a.erdogan2018@gtu.edu.tr>
2021-08-18 16:28:29 +0300

    add id to model objects closes #3

---
+++ pet-clinic-data/src/main/java/com/yusuferdogan/petclinic/model/BaseEntity.java
@@ -0,0 +1,21 @@
+package com.yusuferdogan.petclinic.model;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+public class BaseEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
