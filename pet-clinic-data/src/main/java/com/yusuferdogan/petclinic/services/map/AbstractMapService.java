b3ecd246725d28613c134776638d469471bc975d
yusuf <a.erdogan2018@gtu.edu.tr>
2021-09-04 22:27:11 +0300

    created map services

---
+++ pet-clinic-data/src/main/java/com/yusuferdogan/petclinic/services/map/AbstractMapService.java
@@ -0,0 +1,31 @@
+package com.yusuferdogan.petclinic.services.map;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public abstract class AbstractMapService<T, ID> {
+    protected Map<ID, T> map = new HashMap<>();
+
+    Set<T> findAll() {
+        return new HashSet<>(map.values());
+    }
+
+    T findById(ID id) {
+        return map.get(id);
+    }
+
+    T save(ID id, T object) {
+        return map.put(id, object);
+    }
+
+    void deleteByID(ID id) {
+        map.remove(id);
+    }
+
+    void delete(T object) {
+        map.entrySet().removeIf(idtEntry -> idtEntry.getValue().equals(object));
+    }
+
+}
