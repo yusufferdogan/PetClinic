d81456d9f2e76fa475f0148e222375f2c04c3e03
yusuf <a.erdogan2018@gtu.edu.tr>
2021-09-12 14:24:08 +0300

    crete specliaty and added to vet

--- pet-clinic-data/src/main/java/com/yusuferdogan/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/yusuferdogan/petclinic/model/Vet.java
@@ -1,6 +1,17 @@
 package com.yusuferdogan.petclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person{
 
+    private Set<Speciality> specialties;
 
+
+    public Set<Speciality> getSpecialties() {
+        return specialties;
+    }
+
+    public void setSpecialties(Set<Speciality> specialties) {
+        this.specialties = specialties;
+    }
 }
